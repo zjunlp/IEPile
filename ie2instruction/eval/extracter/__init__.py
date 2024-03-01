f9d1cb10821fce060e9af0588f005db0f02fa5f7
guihonghao <1248614358@qq.com>
2024-03-01 13:34:27 +0800

    add EEAExtracter

--- ie2instruction/eval/extracter/__init__.py
+++ ie2instruction/eval/extracter/__init__.py
@@ -1,4 +1,5 @@
 from .ee_extracter import EEExtracter
+from .eea_extracter import EEAExtracter
 from .eet_extracter import EETExtracter
 from .re_extracter import REExtracter
 from .ner_extracter import NERExtracter
@@ -11,8 +12,10 @@ def get_extracter(task):
         return NERExtracter
     elif task == RE:
         return REExtracter
-    elif task == EE or task == EEA:
+    elif task == EE:
         return EEExtracter
+    elif task == EEA:
+        return EEAExtracter
     elif task == EET:
         return EETExtracter
     elif task == KG:
